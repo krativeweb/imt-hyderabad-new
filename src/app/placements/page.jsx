7d4bf71ffc59d80d5d92d39f6ffd02f98eb9b6de
krativeweb <krativewebdeveloper@gmail.com>
2026-01-02 16:20:48 +0530

    Update page.jsx

--- src/app/placements/page.jsx
+++ src/app/placements/page.jsx
@@ -3,7 +3,7 @@ import PlacementsClient from "@/seo-page/Placements";
 
 // 🟢 Apply faculty page SEO for ALL faculty profiles
 export async function generateMetadata() {
-  return getMetadata("/placements");
+  return getMetadata("/placements", "/api/placement/slug/placement");
 }
 
 export default function Placements() {
