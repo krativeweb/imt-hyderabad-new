dce14d5b6bc694bbe9599c1c7923c8e8b01cf4ac
krativeweb <krativewebdeveloper@gmail.com>
2026-01-27 12:49:34 +0530

    Update page.jsx

--- src/app/pgdm-finance-management/page.jsx
+++ src/app/pgdm-finance-management/page.jsx
@@ -3,7 +3,7 @@ import PgdmFinanceClient from "@/seo-page/PgdmFinance";
 
 // 🟢 Apply faculty page SEO for ALL faculty profiles
 export async function generateMetadata() {
-  return getMetadata("/pgdm-finance-management");
+  return getMetadata("/pgdm-finance-management", "/api/pgdm-finance");
 }
 
 export default function PgdmFinanc() {
