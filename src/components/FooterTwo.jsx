0b564ed3300c37212a9c2102039c760d5edd8c50
krativeweb <krativewebdeveloper@gmail.com>
2026-02-03 18:19:49 +0530

    Update FooterTwo.jsx

--- src/components/FooterTwo.jsx
+++ src/components/FooterTwo.jsx
@@ -7,19 +7,20 @@ export default function Footer() {
       className="footer ttm-bgcolor-darkgrey ttm-textcolor-white clearfix"
       id="footer"
     >
-      <div className="bottom-footer-text text-center">
-        <div className="container">
-          <div className="row">
-            <div className="col-sm-12">
-              <div className="text-center pb-15 mt-3">
-                Copyright © 2025
-                <span className="text-light fw-bold"> IMT HYDERABAD.</span> All
-                Rights Reserved.
-              </div>
-            </div>
-          </div>
+ <div className="bottom-footer-text text-center">
+  <div className="container">
+    <div className="row">
+      <div className="col-sm-12">
+        <div className="text-center pb-15 mt-3">
+          Copyright © {new Date().getFullYear()}
+          <span className="text-light fw-bold"> IMT HYDERABAD.</span> All
+          Rights Reserved.
         </div>
       </div>
+    </div>
+  </div>
+</div>
+
     </footer>
 
 
