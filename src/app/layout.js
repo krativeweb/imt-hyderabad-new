fd4f4fb7ae9ed03d7e14d2142cbe0ade68901da7
krativeweb <krativewebdeveloper@gmail.com>
2026-02-03 18:15:54 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,6 +15,33 @@ import ConditionalHeadAndScripts, {
 import { getMetadata } from "@/utils/getMetadata";
 import Preloader from "@/components/Preloader";
 
+const websiteSchema = {
+  "@context": "https://schema.org",
+  "@type": "WebSite",
+  "name": "IMT Hyderabad",
+  "url": "https://www.imthyderabad.edu.in/",
+  "potentialAction": {
+    "@type": "SearchAction",
+    "target": "https://www.imthyderabad.edu.in/search?q={search_term_string}",
+    "query-input": "required name=search_term_string"
+  }
+};
+
+const collegeSchema = {
+  "@context": "https://schema.org",
+  "@type": "CollegeOrUniversity",
+  "name": "Institute of Management Technology Hyderabad",
+  "alternateName": "IMT Hyderabad",
+  "url": "https://www.imthyderabad.edu.in/",
+  "logo": "https://www.imthyderabad.edu.in/media/IMT-logo.png",
+  "sameAs": [
+    "https://www.facebook.com/IMThyderabad/",
+    "https://www.instagram.com/hyderabadimt/",
+    "https://www.youtube.com/@imthyderabadchannel",
+    "https://www.linkedin.com/school/institute-of-management-technology-hyderabad/"
+  ]
+};
+
 
 export async function generateMetadata() {
   const fallbackSEO = getMetadata("/");
@@ -35,6 +62,8 @@ export async function generateMetadata() {
   };
 }
 
+
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
@@ -54,6 +83,25 @@ export default function RootLayout({ children }) {
     }}
   />
 
+      {/* WebSite Schema */}
+  <Script
+    id="website-schema"
+    type="application/ld+json"
+    strategy="afterInteractive"
+    dangerouslySetInnerHTML={{
+      __html: JSON.stringify(websiteSchema),
+    }}
+  />
+
+  {/* College Schema */}
+  <Script
+    id="college-schema"
+    type="application/ld+json"
+    strategy="afterInteractive"
+    dangerouslySetInnerHTML={{
+      __html: JSON.stringify(collegeSchema),
+    }}
+  />
  
       </head>
       <body>
